1b43cfeb76e75f78b66ecbb7d61f49bd5e01c280
hafsachenni <hafsachenni77@gmail.com>
2023-10-18 14:24:17 -0700

    argc

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - interpreter for monty files
+ * @argc: arg count
+ * @argv: array of arg tokens
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
