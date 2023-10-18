e3af266ce2d8c63bf4335b36215f9c1fe85eaa6c
hafsachenni <hafsachenni77@gmail.com>
2023-10-18 01:32:53 -0700

    no operation

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - doesnt do anything (nop function)
+ * @stack: pointer to a pointer to the stack
+ * @line_number: line number
+ * Return: nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
--- push.c
+++ push.c
@@ -2,4 +2,9 @@
 
 /**
  * push - function that pushes a value to the top of the stack
- * 
+ * @stack: pointer to a pointer of the stack
+ * @line_number: line num representing where the push op will be executed
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
