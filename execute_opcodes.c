042c4ad933ed77c992faa875caa62f2a55f5272a
hafsachenni <hafsachenni77@gmail.com>
2023-10-19 03:55:30 -0700

    execution of opcodes

---
+++ execute_opcodes.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * execute_opcodes - executes monty opcodes
+ * @file: file to be read and interpreted
+ */
+
+void execute_opcodes(FILE *file)
+{
+
+
--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 /**
  * main - interpreter for monty files
  * @argc: arg count
- * @argv: array of arg tokens
+ * @argv: array of strings
  * Return: 0
  */
 
 int main(int argc, char **argv)
 {
-	(void) argv;
+	FILE *file; /*file to open and read from*/
 
 	if (argc != 2)
 	{
@@ -20,4 +20,14 @@ int main(int argc, char **argv)
 	}
 
 	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
 
+
+	execute (file);
+
+	return (0);
+}
