8eb71f8276943fd9f26b8c91d028a166ff39247c
hafsachenni <hafsachenni77@gmail.com>
2023-10-19 08:13:19 -0700

    instructions

--- opcode_func.c
+++ opcode_func.c
@@ -1,12 +1,37 @@
 #include "monty.h"
 
 /**
- * getopcode_func - gets its function its opcode
+ * getopcode_func - gets the opcode  its corresponding function
  * @line_number: line num in monty
  * @opcode: opcode in file
+ * @stack: pointer to the top of stack
  */
 
 void getopcode_func(stack_t **stack, unsigned int line_number, char *opcode)
 {
 	instruction_t operations[] = {
-		{"push", 
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+	}
+
+	int i = 0;
+
+	while (operations[i].opcode != NULL)
+	{
+		if (strcmp(operations[i].opcode, opcode) == 0)
+		{
+			operations[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+
+	fprintf(stderr, "l%u: unknown instruction %s\n", line_number, opcode);
+	free_list(*stack);
+	exit(EXIT_FAILURE);
+}
